91f479fc8aeed49994b995125122cc3f36db1bcf
freimar <freimarsuarez45@gmail.com>
2025-07-29 21:27:54 -0500

    modificacion del nombre

--- codigo.py
+++ codigo.py
@@ -1,4 +1,4 @@
-nombre = "camila"
+nombre = "pepe"
 apellido = "Freimar"
 
 saludo ="hola " + nombre + " " + apellido + " ¿como estas?"
--- codigo2.py
+++ codigo2.py
@@ -1,2 +1,2 @@
-nombre = "camicami"
+nombre = "camilita"
 print("hola " + nombre)
